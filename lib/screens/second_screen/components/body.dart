39c661ae565a6625dda1578669835c791b087429
Brafit2001 <100429071@alumnos.uc3m.es>
2022-10-12 19:09:27 +0200

    Personajes terminados

--- lib/screens/second_screen/components/body.dart
+++ lib/screens/second_screen/components/body.dart
@@ -14,35 +14,38 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
 
-  String _currentMap = "assets/images/Main_Wallpaper.png";
-  String _curentAgent = "";
+  final String _currentMap = "assets/images/Main_Wallpaper.png";
+  String _currentAgent = "";
   
   void _toggleAgent(index){
     setState(() {
       var lista = index.split("/");
-      _curentAgent = "assets/images/Agent_Body/${lista[3]}";
+      _currentAgent = "assets/images/Agent_Body/${lista[3]}";
     });
   }
 
-  bool isCurrentMap(index){
-    if (_currentMap != "assets/images/Main_Wallpaper.png"){
-
-      if (index == _currentMap){return true;}else{return false;}
+  bool isCurrentAgent(index){
+    if (_currentAgent != ""){
+      String name_i = index.split("/")[3].split(".")[0];
+      String name_c = _currentAgent.split("/")[3].split(".")[0];
+      if (name_i == name_c){return true;}else{return false;}
     }
     return false;
   }
 
-  FittedBox AgentTitleGen(String text, double opacity){
-    if (_curentAgent != ""){
+  Text agentTitleGen(String text, double opacity){
+    if (_currentAgent != ""){
       String aux = text.split("/")[3].split(".")[0].toUpperCase();
-      return FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Text(
-            aux,
-            style: tungstenBoldFont(Colors.white, 200, opacity: opacity),));
+      return Text(
+              aux,
+              style: tungstenBoldFont(Colors.white, 200, opacity: opacity),
+      );
     }else{
       String aux = selectedMap.split("/")[2].split(".")[0].toUpperCase();
-      return FittedBox(child: Text(aux, style: tungstenBoldFont(Colors.white, 125, opacity: opacity),));
+      return Text(
+              aux,
+              style: tungstenBoldFont(Colors.white, 125, opacity: opacity),
+      );
     }
   }
 
@@ -50,69 +53,98 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          decoration:  BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(selectedMap), //Background Image
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(child: AgentTitleGen(_curentAgent, 0.7)),
-            Center(child: AgentTitleGen(_curentAgent, 0.6)),
-            Center(child: AgentTitleGen(_curentAgent, 0.5)),
-            Center(child: AgentTitleGen(_curentAgent, 0.4)),
-          ],),
-        Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.black,
-                    Colors.transparent
-                  ]
-              )
-          ) ,
-        ),
-        Center(
-          child: Container(
-            height: 650,
-            decoration:  BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(_curentAgent), //Background Image
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
-
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            margin: const EdgeInsets.only(bottom: 30.0),
-            height: 400,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children:  [
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Text('SELECT YOUR MAP', style: valorantFont(Colors.white, 16),
-
-                    ),
-                  ),
-                  const Divider(),
-                  buildCarouselSlider(),
-                  const Divider(),
-                  const LockInButton(),
-                ]),
-          ),
-        ),
+        buildBackground(),
+        buildAgentNames(),
+        buildGradient(),
+        agentBodyException(),
+        buildBottom(),
       ],
     );
+  }
+
+  Container buildBackground() {
+    return Container(
+        decoration:  BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(selectedMap), //Background Image
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+  }
+
+  Padding buildAgentNames() {
+    return Padding(
+      padding: const EdgeInsets.only(top:30),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: FittedBox(
+          fit: BoxFit.fitHeight,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                agentTitleGen(_currentAgent, 0.7),
+                agentTitleGen(_currentAgent, 0.6),
+                agentTitleGen(_currentAgent, 0.5),
+                agentTitleGen(_currentAgent, 0.4),
+              ],),
+        ),
+      ),
+    );
+  }
+
+  Container buildGradient() {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black,
+                  Colors.transparent
+                ]
+            )
+        ) ,
+      );
+  }
+
+  Center agentBodyException() {
+    if (_currentAgent != ""){
+      return Center(
+        child: Image.asset(
+          _currentAgent,
+          height: 650,
+          fit: BoxFit.cover,
+        ),
+      );
+    }else{
+      return const Center();
+    }
+
+  }
+
+  Align buildBottom() {
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 30.0),
+          height: 400,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children:  [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text('SELECT AGENT', style: valorantFont(Colors.white, 16),
+
+                  ),
+                ),
+                const Divider(),
+                buildCarouselSlider(),
+                const Divider(),
+                const LockInButton(),
+              ]),
+        ),
+      );
   }
 
   CarouselSlider buildCarouselSlider() {
@@ -131,7 +163,6 @@ class _BodyState extends State<Body> {
               onTap: (){
                 _toggleAgent(i);
                 selectedAgent = i;
-                print(i.split("/"));
               },
               child: Container(
                     width: MediaQuery.of(context).size.width,
@@ -140,8 +171,8 @@ class _BodyState extends State<Body> {
                     decoration: BoxDecoration(
                         color: kGray800,
                         border: Border.all(
-                            color: isCurrentMap(i) ? Colors.redAccent : Colors.transparent,
-                            width: isCurrentMap(i) ? 2.0 : 0.0)
+                            color: isCurrentAgent(i) ? Colors.redAccent : Colors.transparent,
+                            width: isCurrentAgent(i) ? 2.0 : 0.0)
                     ),
                     child: Image.asset(i, fit: BoxFit.cover,),
                   )
