f1e52beb87ea10c8353daf4ced2063eb5763a778
PabloRius <100428976@alumnos.uc3m.es>
2023-01-09 21:40:00 +0100

    Pantalla de selección de habilidad reacciona al elegir una de las 4 (falta habilitar que se cambien según el agente que se seleccionó en la pantalla anterior)

--- lib/screens/third_screen/components/body.dart
+++ lib/screens/third_screen/components/body.dart
@@ -1,6 +1,5 @@
 import 'dart:collection';
 
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import '../../../constants.dart';
